bfb0c3bbd5d8cf438df28a82650a40db1ec215bc
SynicalMX <synicalmxc@proton.me>
2026-06-20 14:31:28 -0400

    chore: remove unused import

--- src/client/java/net/playnimbus/nimbusutils/network/Packet.java
+++ src/client/java/net/playnimbus/nimbusutils/network/Packet.java
@@ -1,7 +1,6 @@
 package net.playnimbus.nimbusutils.network;
 
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
